1e09c9428eb8e9343f868836ac6dd659af019343
infzer0 <johnortizo@outlook.com>
2020-03-23 14:58:59 -0500

    Electron - Curso - DOMContentLoaded
    Electron Curso: 14 Codificación de Event Listener para Obtener Versiones de Node.js, Chromium y Electron.

--- App01-Intro/preload.js
+++ App01-Intro/preload.js
@@ -1,3 +1,15 @@
+function establecerVersion(idSelector, version){
+    let elemento = document.getElementById(idSelector);
+
+    if (elemento){
+        elemento.innerText = version;
+    }
+}
+
 window.addEventListener('DOMContentLoaded', () => {
-    
+    const componentes = ['Node', 'Chromium', 'Electron']
+
+    for(const componente of componentes){
+        establecerVersion(`version${componente}`, process.versions[componente]);
+    }
 });
