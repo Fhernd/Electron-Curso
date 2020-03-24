d232918d94e6885d5dd10303a1efd49d9ad2684d
infzer0 <johnortizo@outlook.com>
2020-03-24 18:44:10 -0500

    Electron - Curso - main.js
    Electron Curso: 24 Creación de un Objeto BrowserWindow para Representar la Ventana Principal.

---
+++ App01-Marcadores/main.js
@@ -0,0 +1,12 @@
+const {app, BrowserWindow} = require('electron');
+
+function crearVentanaPrincipal() {
+    let ventanaPrincipal = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+
+    ventanaPrincipal.loadFile('index.html');
+}
+
+app.whenReady().then(crearVentanaPrincipal);
