2e7831f0b49e850127563bf7f56e050df4c7d34d
infzer0 <johnortizo@outlook.com>
2020-03-25 16:15:06 -0500

    Electron - Curso - Ajustes Finales
    Electron Curso: 44 Ajustes Finales sobre el Archivo main.js - Proceso Principal.

--- App01-Marcadores/main.js
+++ App01-Marcadores/main.js
@@ -13,3 +13,15 @@ function crearVentanaPrincipal() {
 }
 
 app.whenReady().then(crearVentanaPrincipal);
+
+app.on('window-all-closed', function(){
+    if (process.platform !== 'darwin'){
+        app.quit();
+    }
+});
+
+app.on('activate', function() {
+    if(BrowserWindow.getAllWindows().length === 0){
+        crearVentanaPrincipal();
+    }
+});
