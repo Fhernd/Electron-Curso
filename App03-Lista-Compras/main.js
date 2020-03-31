6c5f9e14ea6f92db4e4a9c951747a82545037351
infzer0 <johnortizo@outlook.com>
2020-03-31 16:02:22 -0500

    Electron - Curso - Menú Principal
    Electron Curso: 71 Crear el Menú Archivo para la Ventana Principal de la Aplicación.

--- App03-Lista-Compras/main.js
+++ App03-Lista-Compras/main.js
@@ -3,6 +3,37 @@ const {app, BrowserWindow, Menu, ipcMain} = require('electron');
 let ventanaPrincipal;
 let ventanaNuevoProducto;
 
+let menuPrincipalPlantilla = [
+    {
+        label: 'Archivo',
+        submenu: [
+            {
+                label: 'Agregar producto',
+                click() {
+                    crearVentanaAgregarProducto()
+                }
+            },
+            {
+                label: 'Eliminar productos',
+                click() {
+                    ventanaPrincipal.webContents.send('productos:eliminar')
+                }
+            },
+            {
+                label: 'Salir',
+                accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+function crearVentanaAgregarProducto(){
+
+}
+
 function crearVentanaPrincipal(){
     ventanaPrincipal = new BrowserWindow({
         width: 800,
@@ -13,6 +44,9 @@ function crearVentanaPrincipal(){
     });
 
     ventanaPrincipal.loadFile('index.html');
+
+    let menuPrincipal = Menu.buildFromTemplate(menuPrincipalPlantilla);
+    ventanaPrincipal.setMenu(menuPrincipal);
 }
 
-
+app.whenReady().then(crearVentanaPrincipal);
