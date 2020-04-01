3d69f78d78147a89a2f447538122d7ad6fcae9c2
infzer0 <johnortizo@outlook.com>
2020-04-01 15:24:15 -0500

    Electron - Curso - main.js
    Electron Curso: 85 Iniciar la Codificación del Punto de Entrada a la Aplicación - main.js.

---
+++ App04-Grabar-Escritorio/main.js
@@ -0,0 +1,16 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+
+const crearVentanaPrincipal = () => {
+    let ventanaPrincipal = new BrowserWindow({
+        width: 700,
+        height: 650,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    ventanaPrincipal.loadFile(path.join(__dirname, 'index.html'));
+};
+
+app.whenReady().then(crearVentanaPrincipal);
