9d8792ac83aab7a274f62ac9aa964b4a27a40421
infzer0 <johnortizo@outlook.com>
2020-04-01 16:21:41 -0500

    Electron - Curso - Fuentes Captura Vídeo
    Electron Curso: 87 Crear Función Asincrónica para Obtener las Fuentes de Captura de Vídeo.

--- App04-Grabar-Escritorio/js/renderer.js
+++ App04-Grabar-Escritorio/js/renderer.js
@@ -2,7 +2,27 @@ const {desktopCapturer, remote} = require('electron');
 const {writeFile} = require('fs');
 const {dialog, Menu} = remote;
 
+async function obtenerSeleccionarFuentesVideo() {
+    const fuentesEntrada = await desktopCapturer.getSources({types: ['window', 'screen']});
+
+    const menuFuentesEntrada = Menu.buildFromTemplate(
+        fuentesEntrada.map(fuenteEntrada => {
+            return {
+                label: fuenteEntrada.name,
+                click: () => seleccionarFuente(fuenteEntrada)
+            };
+        } )
+    );
+
+    
+}
+
 let grabadorMultimedia;
 const partesGrabacion = [];
 
+let visualizacionCaptura = document.querySelector('#visualizacionCaptura');
+let seleccionarFuenteVideo = document.querySelector('#seleccionarFuenteVideo');
+let grabar = document.querySelector('#grabar');
+let detener = document.querySelector('#detener');
 
+seleccionarFuenteVideo.onclick = obtenerSeleccionarFuentesVideo;
