93ca982f878940b1e3ae771605fbe12b2fd97fe7
infzer0 <johnortizo@outlook.com>
2020-04-15 07:00:12 -0500

    Electron - Curso - new Chart()
    Electron Curso: 101 Crear el Método de Instancia para Crear el Gráfico de la Actividad de CPU con Chart.js.

--- App05-Monitor-Actividad-CPU/js/app.js
+++ App05-Monitor-Actividad-CPU/js/app.js
@@ -37,6 +37,29 @@ class MonitorActividadCpu {
                     'rgba(255, 206, 86, 1)'
                 ]
             }
+
+            datos.push(datosCpu);
         }
+
+        return datos;
+    }
+
+    generarVisualizacion() {
+        this.grafico = new Chart($('#monitorCpuCanvas'), {
+            type: 'doughnut',
+            data: {
+                labels: ['Tiempo de usuario (ms)', 'Tiempo de sistema (ms)', 'Tiempo libre (ms)'],
+                datasets: this.obtenerConjuntoDatos()
+            },
+            options: {
+                maintainAspectRatio: false,
+                title: {
+                    display: true,
+                    text: 'Actividad de CPU',
+                    fontColor: 'rgb(250, 250, 250)',
+                    fontSize: 19
+                }
+            }
+        })
     }
 }
