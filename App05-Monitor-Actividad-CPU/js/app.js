c363285f57bf0898ab163b827717b80a789dbc89
infzer0 <johnortizo@outlook.com>
2020-04-15 06:44:58 -0500

    Electron - Curso - Métodos de Instancia para Obtener Tiempos de cada CPU
    Electron Curso: 100 Definición de los Métodos de Instancia para Capturar los Tiempos de Cada CPU.

--- App05-Monitor-Actividad-CPU/js/app.js
+++ App05-Monitor-Actividad-CPU/js/app.js
@@ -8,5 +8,35 @@ class MonitorActividadCpu {
 
     }
 
-    
+    obtenerMedidasRecientesCpu(cpus) {
+        for (let i = 0; i < cpus.length; i++) {
+            this.medidasRecientesCpu[i] = this.obtenerTiemposCpu(cpus[i]);
+        }
+    }
+
+    obtenerTiemposCpu(cpu) {
+        return [
+            cpu.times.user,
+            cpu.times.sys,
+            cpu.times.idle
+        ];
+    }
+
+    obtenerConjuntoDatos() {
+        let datos = [];
+        const cpus = os.cpus();
+
+        for (let i = 0; i < cpus.length; i++) {
+            const cpu = cpus[i];
+            
+            let datosCpu = {
+                data: this.obtenerTiemposCpu(cpu),
+                backgroundColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)'
+                ]
+            }
+        }
+    }
 }
