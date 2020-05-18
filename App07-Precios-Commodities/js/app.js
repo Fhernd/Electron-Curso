1f48cc7c7975fe7381170d8bc9173b00418b1a7b
infzer0 <johnortizo@outlook.com>
2020-05-18 07:13:36 -0500

    Electron Curso: Uso Función csv.parse()
    Electron Curso: 125 Uso de la Función csv() del Módulo Node.js papaparse para Obtener los Precios en CSV.

--- App07-Precios-Commodities/js/app.js
+++ App07-Precios-Commodities/js/app.js
@@ -12,7 +12,17 @@ function cargarPreciosCommodities() {
     for (const commodity in commodities) {
         url = `https://stooq.com/q/?s=${commodities[commodity]}&f=sd2t2ohlc&h&e=csv`;
 
-        
+        csv.parse(url, {
+            download: true,
+            delimiter: ',',
+            complete: (results) => {
+                const registro = results.data[1];
+                const precioAnterior = parseFloat(registro[3])
+                const precioActual = parseFloat(registro[6]);
+
+                let cambio = Math.round((precioActual - precioAnterior) * 100) / 100;
+            }
+        })
     }
 }
 
