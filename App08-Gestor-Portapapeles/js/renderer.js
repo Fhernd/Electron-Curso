585391ad062c41d976c47f571a81c759c2ac5ae1
infzer0 <johnortizo@outlook.com>
2020-10-07 19:16:55 -0500

    Electron Curso: 142 Navegar a través de los Datos del Historial de Copiar y Pegar del Portapapeles
    Navegar a través de los Datos del Historial de Copiar y Pegar del Portapapeles con el lenguaje de programación Python.

--- App08-Gestor-Portapapeles/js/renderer.js
+++ App08-Gestor-Portapapeles/js/renderer.js
@@ -6,11 +6,39 @@ const historialPortapapeles = new dexie('historial');
 
 let dato = jquery('#dato');
 let tablaPortapapeles = jquery('#tablaPortapapeles');
+tablaPortapapeles.on('click', cambiarDatoSeleccionado);
 
 remote.getCurrentWindow().on('show', () => {
     dato.focus();
 });
 
 jquery('body').on('keydown', function(evento) {
-    // TODO: Actualizar tabla del portapapeles...
+    let filas = Array.from(document.querySelectorAll('tr td:first-child'));
+
+    let indice = filas.indexOf(document.activeElement);
+
+    if (evento.key === 'ArrowUp') {
+        let siguienteDato = filas[indice - 1] || filas[filas.length - 1];
+        siguienteDato.focus();
+    } else if (evento.key === 'ArrowDown') {
+        let siguienteDato = filas[indice + 1] || filas[0];
+        siguienteDato.focus();
+    } else if (evento.key === 'Enter') {
+        cambiarDatoSeleccionado();
+    } else if (evento.key === 'Escape') {
+        dato.value = '';
+        refrescarVista();
+        remote.getCurrentWindow().close();
+    } else {
+        dato.focus();
+        refrescarVista();
+    }
 });
+
+function cambiarDatoSeleccionado() {
+    // TODO...
+}
+
+function refrescarVista() {
+    // TODO...
+}
