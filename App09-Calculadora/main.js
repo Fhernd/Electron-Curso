9505f391102d627a2494a15a2b84e5d9e407d252
infzer0 <johnortizo@outlook.com>
2020-10-31 07:56:20 -0500

    Electron Curso: 164 Terminar con la Implementación del Archivo main.js - Punto de Entrada a la App
    Terminar con la Implementación del Archivo main.js - Punto de Entrada a la App con el lenguaje de programación JavaScript y el framework Lodash.

--- App09-Calculadora/main.js
+++ App09-Calculadora/main.js
@@ -18,4 +18,51 @@ app.on('ready', () => {
     });
 
     ventanaPrincipal.loadURL(`file://${__dirname}/index.html`);
+
+    let plantillaMenu = Menu.buildFromTemplate([
+        {
+            label: 'Opciones',
+            submenu: [
+                {
+                    label: 'Recargar',
+                    accelerator: 'CommandOrControl+R',
+                    role: 'reload'
+                },
+                {
+                    label: 'Salir',
+                    accelerator: 'CommandOrControl+W',
+                    role: 'close'
+                }
+            ]
+        }
+    ]);
+
+    Menu.setApplicationMenu(plantillaMenu);
 });
+
+let menuPrincipal = new Menu();
+menuPrincipal.append(
+    new MenuItem({
+        label: 'Recargar',
+                    accelerator: 'CommandOrControl+R',
+                    role: 'reload'
+    })
+);
+menuPrincipal.append(
+    new MenuItem({
+        label: 'Salir',
+        accelerator: 'CommandOrControl+W',
+        role: 'close'
+    })
+);
+
+app.on('browser-window-created', function(evento, ventana) {
+    ventana.webContents.on('context-menu', function(e, parametros) {
+        menuPrincipal.popup(ventana, parametros.x, parametros.y);
+    });
+});
+
+ipcMain.on('show-context-meenu', function(evento) {
+    let ventana = BrowserWindow.fromWebContents(evento.sender);
+    menuPrincipal.popup(ventana);
+})
