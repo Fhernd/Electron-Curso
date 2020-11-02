9245faaeca713abedb2b2a08b1966c253bbf1c08
infzer0 <johnortizo@outlook.com>
2020-11-02 16:25:15 -0500

    Electron Curso: 165 Inicio Implementación de la Función presionTecla() - Evaluación de una Expresión
    Inicio Implementación de la Función presionTecla() - Evaluación de una Expresión con el lenguaje de programación JavaScript y el framework Electron.

--- App09-Calculadora/js/app.js
+++ App09-Calculadora/js/app.js
@@ -12,9 +12,30 @@ for(let i = 0; i < teclas.length; ++i) {
 document.addEventListener('keydown', pulsacionTeclas)
 
 function presionTecla(evento, tecla) {
+    let valorIngresado = pantalla.textContent;
 
+    if (tecla == 'C') {
+        limpiarPantalla();
+    } else if (tecla == '=') {
+        let ecuacion = valorIngresado;
+        let ultimoCaracter = ecuacion[ecuacion.length - 1];
+
+        if (operadores.indexOf(ultimoCaracter) != -1 || ultimoCaracter == '.') {
+            ecuacion = ecuacion.replace(/.$/, '');
+        }
+
+        if (ecuacion) {
+            pantalla.textContent = eval(ecuacion);
+        }
+
+        puntoPresionado = false;
+    }
 }
 
 function pulsacionTeclas(evento) {
-    
+
+}
+
+function limpiarPantalla() {
+
 }
