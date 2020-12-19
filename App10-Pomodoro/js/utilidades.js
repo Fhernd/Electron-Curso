9f7013a04275b0e5f6e1736192ff1feee84c052e
infzer0 <johnortizo@outlook.com>
2020-12-19 11:54:40 -0500

    Electron Curso: 205 Selección de los Elementos de la Interfaz Gráfica de Usuario con querySelector()
    Selección de los Elementos de la Interfaz Gráfica de Usuario con querySelector() con el legnuaje de programación JavaScript y el framework Electron.

---
+++ App10-Pomodoro/js/utilidades.js
@@ -0,0 +1,10 @@
+module.exports.elementos = {
+    BOTON_CERRAR: document.querySelector('.boton-cerrar'),
+    BOTON_MINIMIZAR: document.querySelector('.boton-minimizar'),
+    BOTON_TEMPORIZADOR: document.querySelector('.boton-temporizador'),
+    BOTON_ACERCA_DE: document.querySelector('.btn-acerca-de'),
+    BOTON_INICIAR: document.querySelector('.boton-iniciar'),
+    BOTON_PAUSAR: document.querySelector('.boton-pausar'),
+    BOTON_GUARDAR: document.querySelector('.boton-guardar'),
+    BOTON_REINICIAR: document.querySelector('.boton-reiniciar')
+}
