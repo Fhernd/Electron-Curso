54d05d9953212499a891d698b93e03ca183e56a3
John Ortiz Ordoñez <johnortizo@outlook.com>
2021-01-21 17:21:57 -0500

    Electron Curso: 213 Acceso al Local Storage para Leer el Tiempo de Configuración de un Break
    Acceso al Local Storage para Leer el Tiempo de Configuración de un Break con el lenguaje de programación JavaScript y el framework Electron.

--- App10-Pomodoro/js/ventana_emergente.js
+++ App10-Pomodoro/js/ventana_emergente.js
@@ -1,4 +1,10 @@
 const {elementos_gui, constantes} = require('./utilidades');
 const {Temporizador} = require('./temporizador');
 
+const DATOS_USUARIO = JSON.parse(localStorage.getItem(constantes.ALMACENAMIENTO_USUARIO));
+let temporizadorBreak = new Temporizador('break', DATOS_USUARIO.break);
 
+elementos_gui.RELOJ_MINUTOS.firstElementChild.innerHTML = DATOS_USUARIO.break;
+elementos_gui.RELOJ_SEGUNDOS.firstElementChild.innerHTML = '00';
+
+temporizadorBreak.iniciar();
